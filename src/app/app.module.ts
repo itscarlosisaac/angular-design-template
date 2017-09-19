dede493a6107606bffc1d7765f158452d37155ab
Carlos I. Martinez <itscarlosisaac@gmail.com>
2017-09-19 03:07:02 -0400

    Added the final touches for the product form.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -18,6 +18,7 @@ import { routes } from './app.routes';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     BrowserAnimationsModule,
     routes
   ],
