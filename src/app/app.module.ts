b9067f8d1b0eb8c004460263b1f30d1104a341a5
Carlos I. Martinez <itscarlosisaac@gmail.com>
2017-09-19 02:20:55 -0400

    Genereate the necessary components for the application.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
